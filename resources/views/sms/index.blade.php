4f783a3675a32f2e557905c618a1d2b303fdbe53
delix86 <vnazarov777@gmail.com>
2016-08-04 15:48:17 +0300

    add sendlogin form

--- resources/views/sms/index.blade.php
+++ resources/views/sms/index.blade.php
@@ -7,12 +7,8 @@
 
             <!-- SMS -->
             <div class="panel panel-default">
-
                 <div class="panel-heading row">
-
                     @include('common.errors')
-
-                    <div class="col-md-4"></div>
 
                     <div class="col-md-4">
                         <h5 class="text-center">Отправить SMS</h5>
@@ -22,6 +18,7 @@
                                 <label for="text" class="control-label">Текст (максимум 500):</label>
                                 <textarea id="text" name="text" class="form-control" rows="8">{{ old('text') }}</textarea>
                             </div>
+                            <!--
                             <div class="form-group">
                                 <label for="users" class="control-label">Кому:</label>
                                 <select id="users" name="recipient_id" class="form-control" value="{{ old('recipient_id') }}">
@@ -32,9 +29,10 @@
                                     @endforeach
                                 </select>
                             </div>
+                            -->
                             <div class="form-group">
                                 <label for="phone" class="control-label">Номер телефона:</label>
-                                <input id="phone" name="phone" class="form-control" value="{{ old('phone') }}"/>
+                                <input id="phone" name="phone" class="form-control" placeholder="+79781234567" value="{{ old('phone') }}"/>
                             </div>
                             <div class="form-group">
                                 <input type="submit" value="Отправить" class="form-control"/>
@@ -42,10 +40,33 @@
                         </form>
                     </div>
 
-                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <h5 class="text-center">Отправить Логин и Пароль</h5>
+                        <form action="/sms/sendlogin" method="post">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="login" class="control-label">Логин:</label>
+                                <input id="login" name="login" class="form-control" value="{{ old('login') }}"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="pass" class="control-label">Пароль:</label>
+                                <input id="pass" name="pass" class="form-control" value="{{ old('pass') }}"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="uid" class="control-label">UID:</label>
+                                <input id="uid" name="uid" class="form-control" value="{{ old('uid') }}"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone" class="control-label">Номер телефона:</label>
+                                <input id="phone" name="phone" class="form-control" placeholder="+79781234567" value="{{ old('phone') }}"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Отправить" class="form-control"/>
+                            </div>
+                        </form>
+                    </div>
 
                 </div>
-
             </div>
 
             <!-- SMS -->
